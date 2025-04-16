16b67582bfe64b99cbe43e40d5d5ffe40b73421b
itrucontent <kaistudents@yandex.ru>
2025-04-16 17:26:53 +0300

    Добавлены новые команды и фильтры

--- echobot.py
+++ echobot.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.enums import ContentType
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, BotCommand
 from aiogram import F
 from dotenv import load_dotenv
 import os
@@ -14,6 +14,15 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 # Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+# Функция для установки команд бота в меню
+async def set_commands():
+    commands = [
+        BotCommand(command="start", description="Запустить бота"),
+        BotCommand(command="help", description="Помощь по использованию бота"),
+        BotCommand(command="caps", description="Преобразовать текст в ЗАГЛАВНЫЕ буквы")
+    ]
+    await bot.set_my_commands(commands)
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
@@ -30,6 +39,35 @@ async def process_help_command(message: Message):
         'я пришлю тебе твое сообщение'
     )
 
+@dp.message(Command(commands=['caps']))
+async def text_caps(message: Message):
+    text_parts = message.text.split(maxsplit=1)
+    if len(text_parts) > 1:
+        text_change = text_parts[1].upper()
+        await message.reply(text_change)
+    else:
+        await message.reply("Введите текст после команды /caps")
+
+@dp.message(Command(commands=['reverse']))
+async def text_reverse(message: Message):
+    text_parts = message.text.split(maxsplit=1)
+    if len(text_parts) > 1:
+        text_change = text_parts[1][::-1]
+        await message.reply(text_change)
+    else:
+        await message.reply("Введите текст после команды /reverse")
+
+@dp.message(F.text.lower().in_(['hi', 'hello', 'салют']) | F.text.startswith("здрав") | F.text.startswith("привет"))
+async def say_hello(message: Message):
+    await message.answer("И вам здравствуйте!")
+
+@dp.message(F.text.isdigit())
+async def good_deel(message: Message):
+    await message.reply("Похоже намечается серьезная сделка?")
+
+@dp.message(F.text.endswith("?"))
+async def send_question(message: Message):
+    await message.reply("Похоже у вас серьезный вопрос")
 
 @dp.message(F.photo)
 async def send_photo(message: Message):
